4de78da7451a5f69f5f83296e1fa38334ed6a68b
sunoneeye <sunoneeye@gmail.com>
2023-04-17 11:02:15 +0800

    1.1_3_6 多次模拟，改用pandas处理数据

--- 1.1_3_6.py
+++ 1.1_3_6.py
@@ -1,8 +1,8 @@
 from temp_3_6 import navi_3_6
 import pickle, csv, sys, time
+import pandas as pd
 
-
-start_time = time.perf_counter()
+start_time = time.time()
 data = []
 size = 500
 with open('ignore/gene.pickle', 'rb') as f:
@@ -16,25 +16,27 @@ with open('ignore/gene.pickle', 'rb') as f:
             print(sys.exc_info())
             break
 
-
 merged_list = []
 
-for i in range(10):
+for i in range(5):
     way, poss = navi_3_6(size, data[0])
     for j in range(4):
         merged_list.append((data[i], way[j], poss[j]))
 
 sorted_data = sorted(merged_list, key=lambda x: x[2], reverse=True)
 
+p = [x[0] for x in sorted_data]
+w = [x[1] for x in sorted_data]
+l = [x[2] for x in sorted_data]
 
-# 将三元组写入到文件中
-with open('ignore/test.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['parents', 'way', 'likely_hood'])  # 写入列名
-    for i in range(len(sorted_data)):
-        writer.writerow(sorted_data[i])  # 写入三元组数据
+data = {'parents': p,
+        'way': w,
+        'likelihood': l}
 
+df = pd.DataFrame(data)
+df.to_csv('output.csv', index=False)
 
-end_time = time.perf_counter()
-total_time = end_time - start_time
-print("程序运行时间为：", total_time, "秒")
+end_time = time.time()
+
+with open('time.txt', 'w') as f:
+    f.write(str(end_time - start_time))
--- temp_3_6.py
+++ temp_3_6.py
@@ -4,34 +4,36 @@ from Constant import Gene
 
 def navi_3_6(num, p):
     """
+    3个亲本6对基因组的育种模版
 
-
-    :param num:
-    :param p:
-    :return:
+    :param num:种群数量
+    :param p:育种材料
+    :return:育种路径和纯和子概率
     """
     poss = []
     way = []
     for i in range(1, 5):
 
         j = 5 - i
+        like = 0
+        for times in range(100):
+            # 子一代
+            f_gene = Gene().add_gene(p[0], p[1])
 
-        # 子一代
-        f_gene = Gene().add_gene(p[0], p[1])
+            # 子一代多次自交
+            for x in range(i):
+                f_gene = inbred(num, f_gene)
 
-        # 子一代多次自交
-        for x in range(i):
-            f_gene = inbred(num, f_gene)
+            # 子一代和另一个亲本i次回交
 
-        # 子一代和另一个亲本i次回交
+            f_gene = hybrid(num, Gene().add_gene(p[2]), f_gene)
 
-        f_gene = hybrid(num, Gene().add_gene(p[2]), f_gene)
+            # 子二代j次自交
+            for x in range(j):
+                f_gene = inbred(num, f_gene)
 
-        # 子二代j次自交
-        for x in range(j):
-            f_gene = inbred(num, f_gene)
-
-        poss.append(find_pure(num, f_gene)/num)
+            like += find_pure(num, f_gene)
+        poss.append(round(like/num/100, 4))
         way.append('{n}次子一代自交和{m}次子二代自交'.format(n=i, m=j))
 
     return way, poss
